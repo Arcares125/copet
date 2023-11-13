ca7322c3d69df81d0f837c9fbe1541565da5a290
Michael <christian.mandala125@gmail.com>
2023-11-14 06:57:51 +0700

    add variable in config + update models index

--- config/config.js
+++ config/config.js
@@ -1,25 +1,28 @@
 
 require('dotenv').config()
-module.exports = {
+module.exports = {//
   "development": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.PORT
   },
   "test": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.PORT
   },
   "production": {
     "username": process.env.DB_USERNAME,
     "password": process.env.DB_PASS,
     "database": process.env.DB_NAME,
     "host": process.env.DB_HOST,
-    "dialect": process.env.DB_DIALECT
+    "dialect": process.env.DB_DIALECT,
+    "port": process.env.PORT
   }
 }
