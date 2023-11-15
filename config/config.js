c4a36ee13cf4d56ce2fedfe8e8e11e6556d14b8d
Michael <christian.mandala125@gmail.com>
2023-11-15 10:01:47 +0700

    tes login

--- config/config.js
+++ config/config.js
@@ -2,12 +2,18 @@
 require('dotenv').config()
 module.exports = {
   "development": {
+    // "username": process.env.DB_USERNAME,
+    // "password": process.env.DB_PASS,
+    // "database": process.env.DB_NAME,
+    // "host": process.env.DB_HOST,
+    // "dialect": process.env.DB_DIALECT,
+    // "port": process.env.DB_PORT
     "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASS,
-    "database": process.env.DB_NAME,
-    "host": process.env.DB_HOST,
+    "password": process.env.PGPASSWORD,
+    "database": process.env.PGDATABASE,
+    "host": process.env.PGHOST,
     "dialect": process.env.DB_DIALECT,
-    "port": process.env.DB_PORT
+    "port": process.env.PGPORT
   },
   "test": {
     "username": process.env.DB_USERNAME,
