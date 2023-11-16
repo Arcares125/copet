b50bdd406cf7f50d83f0299d73a8f1440f6cca33
Michael <christian.mandala125@gmail.com>
2023-11-16 16:09:04 +0700

    request gowel  service toko card detail

--- controllers/Toko.js
+++ controllers/Toko.js
@@ -148,8 +148,140 @@ const getDetailCardToko = async (req, res) => {
     }
 }
 
+const getDetailCardTokoFull = async (req, res) => {
+
+    const value = req.params
+    let query;
+
+    try {
+        // query = `
+        // SELECT a.id, a.nama as pet_shop_name,z.rating, z.total_rating,
+        // a.deskripsi, a.lokasi, x.service_detail_grooming, x.title_grooming,x.price_grooming,
+        // x.service_detail_hotel, x.title_hotel,x.price_hotel
+        // from (select CAST(AVG(a.rating) AS DECIMAL(10,2)) AS rating,
+        //     COUNT(a.id) as total_rating
+        //     from review a join "order" b ON a.order_id = b.id ) z,
+        //         (SELECT CASE
+        //         WHEN count(b.id) > 0 AND count(c.id) > 0 THEN 'Grooming, Hotel'
+        //         WHEN count(b.id) > 0 AND count(c.id) <= 0 THEN 'Hotel'
+        //         ELSE 'Grooming'
+        //         END AS service, y.title_hotel, y.price_hotel, y.service_detail_hotel,
+        //         k.title_grooming, k.price_grooming, k.service_detail_grooming
+        //         FROM toko a LEFT JOIN hotel b ON a.id = b.toko_id
+        //         LEFT JOIN grooming c ON a.id = c.toko_id, 
+        //             (SELECT tipe_hotel as title_hotel, harga as price_hotel, fasilitas as service_detail_hotel FROM hotel) y,
+        //             (SELECT tipe as title_grooming, harga as price_grooming, fasilitas as service_detail_grooming FROM grooming) k
+        //             GROUP BY y.title_hotel, y.price_hotel, y.service_detail_hotel, 
+        //             k.title_grooming, k.price_grooming, k.service_detail_grooming) x,
+        // toko a LEFT JOIN hotel b
+        // ON a.id = b.toko_id 
+        // LEFT JOIN grooming c ON a.id = c.toko_id
+        // WHERE a.id = :idToko
+        // GROUP BY a.id, a.nama, a.foto,z.rating, z.total_rating,
+        // x.service_detail_grooming, x.title_grooming,x.price_grooming,
+        // x.service_detail_hotel, x.title_hotel,x.price_hotel
+        // `
+        query = `
+        SELECT a.id, a.nama as pet_shop_name,z.rating, z.total_rating,
+        CASE
+            WHEN count(b.id) > 0 AND count(c.id) > 0 THEN 'Grooming, Hotel'
+            WHEN count(b.id) > 0 AND count(c.id) <= 0 THEN 'Hotel'
+            ELSE 'Grooming'
+        END AS service,
+		JSON_BUILD_OBJECT(
+			'hotel', (
+			  SELECT JSON_AGG(
+				  JSON_BUILD_OBJECT(
+					'title', h.tipe_hotel,
+					'price', h.harga::varchar,
+					'service_detail', h.fasilitas
+				  )
+			  )
+			  FROM hotel AS h
+			  WHERE h.toko_id = :idToko
+			),
+			'grooming', (
+			  SELECT JSON_AGG(
+				  JSON_BUILD_OBJECT(
+					'title', g.tipe,
+					'price', g.harga::varchar,
+					'service_detail', g.fasilitas
+				  )
+			  )
+			  FROM grooming AS g
+			  WHERE g.id = :idToko
+			)
+		  ) AS service
+        from (select CAST(AVG(a.rating) AS DECIMAL(10,2)) AS rating,
+            COUNT(a.id) as total_rating
+            from review a join "order" b ON a.order_id = b.id ) z,
+        toko a LEFT JOIN hotel b
+        ON a.id = b.toko_id 
+        LEFT JOIN grooming c ON a.id = c.toko_id
+        GROUP BY a.id, a.nama, a.foto,z.rating, z.total_rating
+        `
+        const detail = await sequelize.query(query, 
+            { 
+                replacements: {
+                    idToko: value.id
+                },
+                type: QueryTypes.SELECT 
+            }
+        )
+
+        // for (const service of detail) {
+        //     const servicesString = service.services.replace('[', '').replace(']', '');
+          
+        //     const servicesArray = servicesString.split(', ');
+          
+        //     service.services = servicesArray;
+        //   }
+
+        return res.status(200).json({
+            message: "Data Detail Toko Grooming dan Hotel berhasil diambil",
+            kode: 200,
+            data: detail
+        })
+    } catch (error) {
+        return res.status(500).json({
+            message: error.message,
+            kode: 500,
+        })
+    }
+}
+
 module.exports = {
     registerToko,
     getDataToko,
-    getDetailCardToko
+    getDetailCardToko,
+    getDetailCardTokoFull
 }
+
+// SELECT
+//   a.id,
+//   a.a_name,
+//   a.services,
+//   (
+//     SELECT MIN(service.price)
+//     FROM service
+//     WHERE service.pet_shop_id = pet_shop.id
+//   ) AS start_from,
+//   (
+//     SELECT AVG(rating)
+//     FROM rating
+//     WHERE rating.pet_shop_id = pet_shop.id
+//   ) AS rating,
+//   (
+//     SELECT COUNT(*)
+//     FROM rating
+//     WHERE rating.pet_shop_id = pet_shop.id
+//   ) AS total_rating,
+//   a.pet_shop_picture,
+//   a.description,
+//   a.location,
+//   (
+//     SELECT json_agg(service)
+//     FROM service
+//     WHERE service.pet_shop_id = pet_shop.id
+//   ) AS service
+// FROM toko a
--- routes/toko.js
+++ routes/toko.js
@@ -1,12 +1,12 @@
 var express = require('express');
-const { registerToko, getDataToko, getDetailCardToko } = require('../controllers/Toko');
+const { registerToko, getDataToko, getDetailCardToko, getDetailCardTokoFull } = require('../controllers/Toko');
 var router = express.Router();
 
 /* GET users listing. */
 router.post('/register', registerToko)
 router.get('/data-toko', getDataToko)
 router.get('/toko-card', getDetailCardToko)
-
+router.get('/toko-card-detail/:id', getDetailCardTokoFull)
 
 
 module.exports = router;
