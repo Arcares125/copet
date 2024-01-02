6d88c4d118c82dcc56140133519d3e8731e52b34
Michael <christian.mandala125@gmail.com>
2024-01-02 10:19:45 +0700

    update routes createActivity

--- routes/pet_activity.js
+++ routes/pet_activity.js
@@ -6,7 +6,7 @@ var router = express.Router();
 
 /* GET users listing. */
 // router.get('/:id?', getHewanPeliharaan)
-router.post('/createActivity', createActivity)
+router.post('/createActivity/:userId?', createActivity)
 router.put('/updateActivity/:activityId?', updateActivity)
 router.get('/getListActivity/:userId?', getListActivity),
 router.delete('/deleteActivity/:activityId?', deleteActivity)
