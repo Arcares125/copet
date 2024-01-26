d8aa551b271aa2137b6368442943c5b01f3ebdef
Michael <christian.mandala125@gmail.com>
2024-01-26 11:21:07 +0700

    add validation showing only avail and accepted dokter trainer

--- controllers/Trainer.js
+++ controllers/Trainer.js
@@ -120,7 +120,7 @@ const getDataTrainer = async (req, res) => {
     try {
         let tempDataTrainer = []
         for(let i = 0; i < dataTrainer.length; i++){
-            if(dataTrainer[i].dataValues.is_acc){
+            if(dataTrainer[i].dataValues.is_acc && dataTrainer[i].dataValues.is_available){
                 tempDataTrainer.push(dataTrainer[i])
             } else {
                 continue
