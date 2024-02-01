67815401d236ae1139c9a6b854238fccd9f73fe2
Michael <christian.mandala125@gmail.com>
2024-02-01 15:23:19 +0700

    add routes delete trainer

--- routes/trainer.js
+++ routes/trainer.js
@@ -1,5 +1,5 @@
 var express = require('express');
-const { registerTrainer, getDataTrainer, updateTrainer, confirmOrder, getDataTrainerDetail, updateAvailable, getDataTrainerAvailable } = require('../controllers/Trainer');
+const { registerTrainer, getDataTrainer, updateTrainer, confirmOrder, getDataTrainerDetail, updateAvailable, getDataTrainerAvailable, deleteTrainer } = require('../controllers/Trainer');
 var router = express.Router();
 
 /* GET users listing. */
@@ -9,6 +9,7 @@ router.get('/data-trainer/:trainerId?', getDataTrainer)
 router.get('/data-trainer-detail/:trainerId?', getDataTrainerDetail)
 router.get('/data-trainer-available', getDataTrainerAvailable)
 router.put('/update/:trainerId', updateTrainer)
+router.delete('/delete/:trainerId', deleteTrainer)
 router.put('/update-status/:trainerId', updateAvailable)
 
 
